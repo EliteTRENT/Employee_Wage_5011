c2e83ad8413d1ff0ff1da5cef2001c4ee995e6ab
Aryan Negi <negiaryan2803@gmail.com>
2024-12-05 09:54:32 +0530

    [Aryan] Add: Adding Task 6

--- Employee.rb
+++ Employee.rb
@@ -3,14 +3,19 @@ part_time = 4
 full_time = 8
 rng = Random.new
 monthly_wage = 0
+total_hours = 100
 for i in 1..20
   number = rng.rand(0..2)
-  if number == 0
+  if total_hours <= 0
+    break
+  elsif number == 0
     monthly_wage += 0
   elsif number == 1
     monthly_wage += (part_time*wage_per_hr)
+    total_hours -= part_time
   else
     monthly_wage += (full_time*wage_per_hr)
+    total_hours -= full_time
   end
 end
 puts "Monthly Wage: $#{monthly_wage}"
