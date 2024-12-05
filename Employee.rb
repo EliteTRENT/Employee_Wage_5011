36914171abc07ccb29e8ccc405ac67b4329d67bc
Aryan Negi <negiaryan2803@gmail.com>
2024-12-05 09:57:01 +0530

    [Aryan] Add: Adding Task 7

--- Employee.rb
+++ Employee.rb
@@ -1,21 +1,40 @@
-wage_per_hr = 20
-part_time = 4
-full_time = 8
-rng = Random.new
-monthly_wage = 0
-total_hours = 100
-for i in 1..20
-  number = rng.rand(0..2)
-  if total_hours <= 0
-    break
-  elsif number == 0
-    monthly_wage += 0
-  elsif number == 1
-    monthly_wage += (part_time*wage_per_hr)
-    total_hours -= part_time
-  else
-    monthly_wage += (full_time*wage_per_hr)
-    total_hours -= full_time
+class Employee
+  @@wage_per_hr = 20
+  @@part_time = 4
+  @@full_time = 8
+  def initialize(type)
+    @type = type
+    @monthly_sal = 0
+  end
+
+  def cal_daily_wage()
+    if @type == 1
+      return "$0"
+    elsif @type == 2
+      return "$#{4*20}"
+    else
+      return "$#{8*20}"
+    end
+  end
+
+  def cal_monthly_wage()
+    for i in 1..20
+      rng = Random.new
+      number = rng.rand(1..3)
+      if number == 1
+        @monthly_sal += 0
+      elsif number == 2
+        @monthly_sal += @@part_time*@@wage_per_hr
+      else 
+        @monthly_sal += @@full_time*@@wage_per_hr
+      end
+    end
+    return "$#{@monthly_sal}"
   end
 end
-puts "Monthly Wage: $#{monthly_wage}"
+
+rng = Random.new
+number = rng.rand(1..3)
+emp = Employee.new(number)
+puts "Daily Wage: #{emp.cal_daily_wage}"
+puts "Monthly Wage: #{emp.cal_monthly_wage}"
